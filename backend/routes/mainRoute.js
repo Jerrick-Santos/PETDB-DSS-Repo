2f471a7ebe82411517e093ac0a21e680f4bed1b5
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2023-08-18 00:52:44 +0800

    added jerrick endpoints

--- backend/routes/mainRoute.js
+++ backend/routes/mainRoute.js
@@ -1,20 +1,59 @@
 const express = require('express');
 const router = express.Router();
 
-// Define your route handlers using the db connection
-router.post('/newpatient', (req, res) => {
 
-    const {last_name, first_name, middle_initial, age, sex} = req.body //kinuha mo lang ung inputs/fields sa react
-
-    db.query('INSERT INTO table_name (last_name, first_name, middle_initial, age, sex) VALUES (?, ?, ?, ?, ?)', 
-    [last_name, first_name, middle_initial, age, sex])
-
-});
-
-// ... Define more route handlers using the 'db' connection
-
-// Export the router with the attached 'db' connection
 module.exports = (db) => {
-    // Attach the 'db' connection to all route handlers before returning the router
+    // Define your route handlers using the db connection
+    router.get('/allpatients', (req, res) => {
+
+        db.query(`
+        SELECT pt.PatientNo, CONCAT(pt.first_name, ' ', pt.last_name) AS fullname,
+            pt.birthdate,
+            pt.sex,
+            pt.age,
+            pc.CaseNo
+        FROM PEDTBDSS_new.TD_PTINFORMATION pt
+        JOIN PEDTBDSS_new.TD_PTCASE pc ON pt.PatientNo = pc.PatientNo;
+    `, (err, results) => {
+        if (err) {
+            console.log(err)
+        } else {
+            results.forEach(result => {
+                console.log(result.age);
+            });
+            res.send(results)
+        }
+    });
+    });
+
+    router.get('/patient/:id', (req, res) => {
+        const id = req.params.id;
+        console.log(id)
+        db.query(`
+            SELECT pt.PatientNo, CONCAT(pt.first_name, ' ', pt.last_name) AS fullname,
+            pt.birthdate,
+            pt.sex,
+            pt.age,
+            pc.CaseNo
+        FROM PEDTBDSS_new.TD_PTINFORMATION pt
+        JOIN PEDTBDSS_new.TD_PTCASE pc ON pt.PatientNo = pc.PatientNo
+        WHERE pt.PatientNo = ${id};
+    `, (err, results) => {
+        if (err) {
+            console.log(err)
+        } else {
+            results.forEach(result => {
+                 result.fullname;
+            });
+            res.send(results)
+        }
+    });
+    });
+
+
+    // ... Define more route handlers using the 'db' connection
+
     return router;
 };
+
+
