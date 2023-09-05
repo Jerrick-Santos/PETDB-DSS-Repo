c5c183308c20deb1d38a2a57542fecce0b99692f
migzprz <134031394+migzprz@users.noreply.github.com>
2023-09-05 22:55:47 +0800

    treatments initial and similar cases

--- frontend/src/pages/SimilarCases.js
+++ frontend/src/pages/SimilarCases.js
@@ -16,6 +16,7 @@ import PresumptiveTBModal from '../components/PresumptiveTBModal';
 import LatentTBModal from '../components/LatentTBModal';
 import CaseHeader from '../components/CaseHeader'
 import { PieChart, Pie, Cell, Label, Legend, Tooltip, ResponsiveContainer, XAxis, YAxis, CartesianGrid, } from 'recharts';
+import Badge from 'react-bootstrap/Badge';
 
 
 const SimilarCases = () => {
@@ -26,109 +27,36 @@ const SimilarCases = () => {
   var caseNum = id
 
   const [casesData, setCasesData] = useState([]);
-  const [percent1, setPercent1] = useState(0);
-  const [percent2, setPercent2] = useState(0);
-  const [percent3, setPercent3] = useState(0);
-  const [caseNum1, setCaseNum1] = useState(0);
-  const [caseNum2, setCaseNum2] = useState(0);
-  const [caseNum3, setCaseNum3] = useState(0);
-
-  const [case1Data, setCase1Data] = useState([]);
-  const [case2Data, setCase2Data] = useState([]);
-  const [case3Data, setCase3Data] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://localhost:4000/api/getsimcases/${caseNum}`)
+    axios.get(`http://localhost:4000/api/patientassessment/${caseNum}`)
+    .then((response) => {
+      
+      if(response.data.length>0){
+
+      axios.get(`http://localhost:4000/api/getsimcases2/${caseNum}`)
       .then(response => {
         setCasesData(response.data);
   
-        // Loop through casesData to populate variables
-        for (let i = 0; i < response.data.length; i++) {
-          const caseIndex = response.data[i][0];
-          const similarity = response.data[i][1];
-  
-          // Assign values to percent1, percent2, percent3, caseNum1, caseNum2, caseNum3
-          if (i === 0) {
-            setPercent1(similarity);
-            setCaseNum1(caseIndex);
-          } else if (i === 1) {
-            setPercent2(similarity);
-            setCaseNum2(caseIndex);
-          } else if (i === 2) {
-            setPercent3(similarity);
-            setCaseNum3(caseIndex);
-          }
-        }
       })
       .catch(error => {
         console.error('Error fetching Sim Cases:', error);
       });
+       }
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the request
+      console.error('Error fetching data:', error);
+    });
+
+    
   }, []);
   
-  useEffect(() => {
-    if (caseNum1) {
-      axios.get(`http://localhost:4000/api/case/${caseNum1}`)
-        .then(response => {
-          setCase1Data(response.data[0]);
-        })
-        .catch(error => {
-          console.error('Error fetching Case 1 Data:', error);
-        });
-    }
-  }, [caseNum1]);
+ 
   
-  useEffect(() => {
-    if (caseNum2) {
-      axios.get(`http://localhost:4000/api/case/${caseNum2}`)
-        .then(response => {
-          setCase2Data(response.data[0]);
-        })
-        .catch(error => {
-          console.error('Error fetching Case 2 Data:', error);
-        });
-    }
-  }, [caseNum2]);
-  
-  useEffect(() => {
-    if (caseNum3) {
-      axios.get(`http://localhost:4000/api/case/${caseNum3}`)
-        .then(response => {
-          setCase3Data(response.data[0]);
-        })
-        .catch(error => {
-          console.error('Error fetching Case 3 Data:', error);
-        });
-    }
-  }, [caseNum3]);
-  
-  console.log(case1Data)
+  console.log(casesData)
   
   // Make sure to check if casesData is available before using it
- 
-    const graphdata1 = [
-    { id: "1", name: "L1", value: 18 },
-    { id: "2", name: "L2", value: 82 }
-    ];
-
-  const graphdata2 = [
-      { id: "1", name: "L1", value: 93},
-      { id: "2", name: "L2", value: 7 }
-      ];
-
-  const graphdata3 = [
-        { id: "1", name: "L1", value: 96 },
-        { id: "2", name: "L2", value: 4 }
-        ];
-  
-        const graphdata4 = [
-          { id: "1", name: "L1", value: 98.5 },
-          { id: "2", name: "L2", value: 1.5 }
-          ];
-
-  
-  
-  
-    const [activeTab, setActiveTab] = useState('1'); 
   return (
     <div>
     <NavBar/>
@@ -184,7 +112,7 @@ const SimilarCases = () => {
       
     <Row className="mb-3">
       <Col className="text-center">
-      <h2> Similar Cases </h2>
+      <h2> Diagnosis Likelihood (Similar Cases) </h2>
       </Col>
     </Row>
     
@@ -193,218 +121,65 @@ const SimilarCases = () => {
      <Row className="mt-5 justify-content-center">
      
 
-     <Col lg="4">
-     <Card className="mb-4">
-       <Card.Body>
-         <Row>
-           <Col sm="4">
-             <Card.Text>Similar Cases</Card.Text>
-           </Col>
-           <Col sm="8">
-             <Card.Text className="text-muted ">121 Cases</Card.Text>
-           </Col>
-         </Row>
-         <hr />
-        
-         <Row>
-           <Col sm="4">
-             <Card.Text>Diagnosis</Card.Text>
-           </Col>
-           <Col sm="8">
-             <Card.Text className="text-muted ">Bacteriologically Confirmed - Drug Resistant</Card.Text>
-           </Col>
-         </Row>
-       
-         
-       </Card.Body>
-     </Card>
+     <Col lg="11">
 
-     </Col>
-     <Col lg="2">
-           <PieChart width={150} height={150}>
-         <Pie
-           data={graphdata1}
-           isAnimationActive={true}
-           dataKey="value"
-           innerRadius="80%"
-           outerRadius="100%"
-           fill='#56592F'
-           startAngle={90}
-           endAngle={-270}
-           paddingAngle={0}
-           cornerRadius={5}
-         >
-           <Cell key="test" fill="#CCC" />
-           <Label
-             value={`${graphdata1[1].value}% Likelihood`}
-             position="center"
-             fontSize={15}
-             fill="#000"
-           />
-         </Pie>
-       </PieChart>
-     </Col>
-
-     <Col lg="4">
-     <Card className="mb-4">
-       <Card.Body>
-         <Row>
-           <Col sm="4">
-             <Card.Text>Similar Cases</Card.Text>
-           </Col>
-           <Col sm="8">
-             <Card.Text className="text-muted ">121 Cases</Card.Text>
-           </Col>
-         </Row>
-         <hr />
-        
-         <Row>
-           <Col sm="4">
-             <Card.Text>Diagnosis</Card.Text>
-           </Col>
-           <Col sm="8">
-             <Card.Text className="text-muted ">Clinically Diagnosed - Drug Resistant</Card.Text>
-           </Col>
-         </Row>
-       
-         
-       </Card.Body>
-     </Card>
-
-     </Col>
-     <Col lg="2">
-           <PieChart width={150} height={150}>
-         <Pie
-           data={graphdata2}
-           isAnimationActive={true}
-           dataKey="value"
-           innerRadius="80%"
-           outerRadius="100%"
-           fill='#BECB12'
-           startAngle={90}
-           endAngle={-270}
-           paddingAngle={0}
-           cornerRadius={5}
-         >
-           <Cell key="test" fill="#CCC" />
-           <Label
-             value={`${graphdata2[1].value}% Likelihood`}
-             position="center"
-             fontSize={15}
-             fill="#000"
-           />
-         </Pie>
-       </PieChart>
-     </Col>
+    
+   <Card className="mb-4">
+   <Card.Body>
+   <Row>
+       <Col sm="7">
+         <Card.Text> <strong>Diagnosis </strong></Card.Text>
+       </Col>
+       <Col sm="5">
+         <Card.Text> <strong>Percentage Likelihood </strong></Card.Text>
+       </Col>
+     </Row>
+     <hr/>
+    
+     <Row>
+       <Col sm="7">
+         <Card.Text className="text-muted "> Bacteriologically Confirmed - Drug Resistant</Card.Text>
+       </Col>
+       <Col sm="5">
+         <Card.Text> <strong>{casesData.bd_res}% </strong></Card.Text>
+       </Col>
+     </Row>
+     <hr/> 
+     <Row>
+       <Col sm="7">
+         <Card.Text className="text-muted "> Bacteriologically Confirmed - Drug Sensitive</Card.Text>
+       </Col>
+       <Col sm="5">
+         <Card.Text> <strong>{casesData.bd_nonres}%  </strong></Card.Text>
+       </Col>
+     </Row>
+     <hr/> 
+     <Row>
+       <Col sm="7">
+         <Card.Text className="text-muted "> Clinically Diagnosed</Card.Text>
+       </Col>
+       <Col sm="5">
+         <Card.Text> <strong>{casesData.clinical_diag}%  </strong></Card.Text>
+       </Col>
+     </Row>
+    <hr/> 
+     <Row>
+       <Col sm="7">
+         <Card.Text className="text-muted "> No Tuberculosis</Card.Text>
+       </Col>
+       <Col sm="5">
+         <Card.Text> <strong>{casesData.no_tb}%  </strong></Card.Text>
+       </Col>
+     </Row>
+   
      
-   </Row>
+   </Card.Body>
+ </Card>
 
-   <Row className="mt-5 justify-content-center">
-     
-
-     <Col lg="4">
-     <Card className="mb-4">
-       <Card.Body>
-         <Row>
-           <Col sm="4">
-             <Card.Text>Similar Cases</Card.Text>
-           </Col>
-           <Col sm="8">
-             <Card.Text className="text-muted ">121 Cases</Card.Text>
-           </Col>
-         </Row>
-         <hr />
-        
-         <Row>
-           <Col sm="4">
-             <Card.Text>Diagnosis</Card.Text>
-           </Col>
-           <Col sm="8">
-             <Card.Text className="text-muted ">EPTB Bacteriologically Confirmed - Drug Susceptible</Card.Text>
-           </Col>
-         </Row>
-       
-         
-       </Card.Body>
-     </Card>
+    
 
      </Col>
-     <Col lg="2">
-           <PieChart width={150} height={150}>
-         <Pie
-           data={graphdata3}
-           isAnimationActive={true}
-           dataKey="value"
-           innerRadius="80%"
-           outerRadius="100%"
-           fill='#7D3D07'
-           startAngle={90}
-           endAngle={-270}
-           paddingAngle={0}
-           cornerRadius={5}
-         >
-           <Cell key="test" fill="#CCC" />
-           <Label
-             value={`${graphdata3[1].value}% Likelihood`}
-             position="center"
-             fontSize={15}
-             fill="#000"
-           />
-         </Pie>
-       </PieChart>
-     </Col>
-
-     <Col lg="4">
-     <Card className="mb-4">
-       <Card.Body>
-         <Row>
-           <Col sm="4">
-             <Card.Text>Similar Cases</Card.Text>
-           </Col>
-           <Col sm="8">
-             <Card.Text className="text-muted ">121 Cases</Card.Text>
-           </Col>
-         </Row>
-         <hr />
-        
-         <Row>
-           <Col sm="4">
-             <Card.Text>Diagnosis</Card.Text>
-           </Col>
-           <Col sm="8">
-             <Card.Text className="text-muted ">No TB</Card.Text>
-           </Col>
-         </Row>
-       
-         
-       </Card.Body>
-     </Card>
-
-     </Col>
-     <Col lg="2">
-           <PieChart width={150} height={150}>
-         <Pie
-           data={graphdata4}
-           isAnimationActive={true}
-           dataKey="value"
-           innerRadius="80%"
-           outerRadius="100%"
-           fill='#0077B6'
-           startAngle={90}
-           endAngle={-270}
-           paddingAngle={0}
-           cornerRadius={5}
-         >
-           <Cell key="test" fill="#CCC" />
-           <Label
-             value={`${graphdata4[1].value}% Likelihood`}
-             position="center"
-             fontSize={15}
-             fill="#000"
-           />
-         </Pie>
-       </PieChart>
-     </Col>
+    
      
    </Row>
 
