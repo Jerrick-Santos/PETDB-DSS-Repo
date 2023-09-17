75daaee5355122aa0795962884ca0aec469f756a
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2023-09-17 20:26:57 +0800

    implemented user permission roles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,34 @@ import PatientSummaryEPTB from './pages/Home/PatientSummaryEPTB';
 import PatientSummaryClosed from './pages/Home/PatientSummaryClosed';
 import SimilarCases from './pages/SimilarCases';
 import TestRoute from './pages/TestRoute';
+import ProtectedRoute from './ProtectedRoute';
+import withAccessControl from './withAccessControl';
+
+{/* admin routes */}
+const AdminLabTestwithAccess = withAccessControl(AdminLabTest, ['admin'])
+const AdminHIWithAccess = withAccessControl(AdminHI, ['admin']);
+const AdminBHCWithAccess = withAccessControl(AdminBHC, ['admin']);
+const HIInfoWithAccess = withAccessControl(HIInfo, ['admin']);
+const BHCInfowithAccess = withAccessControl(BHCInfo, ['admin'])
+
+{/* BHW routes */}
+const PatientSummaryWithAccess = withAccessControl(PatientSummary, ['BHW']);
+const PatientSummaryPTBWithAccess = withAccessControl(PatientSummaryPTB, ['BHW']);
+const PatientSummaryEPTBWithAccess = withAccessControl(PatientSummaryEPTB, ['BHW']);
+const PatientSummaryClosedWithAccess = withAccessControl(PatientSummaryClosed, ['BHW']);
+
+const PatientInfoWithAccess = withAccessControl(PatientInfo, ['BHW']);
+const CloseContactsWithAccess = withAccessControl(CloseContacts, ['BHW']);
+const AssessmentWithAccess = withAccessControl(Assessment, ['BHW']);
+const LaboratoryTestWithAccess = withAccessControl(LaboratoryTest, ['BHW']);
+const DiagnosisWithAccess = withAccessControl(Diagnosis, ['BHW']);
+const TreatmentsWithAccess = withAccessControl(Treatments, ['BHW']);
+const SimilarCasesWithAccess = withAccessControl(SimilarCases, ['BHW']);
+const AddPatientWithAccess = withAccessControl(AddPatient, ['BHW']);
+const ViewPatientWithAccess = withAccessControl(ViewPatient, ['BHW']);
+const CaseWithAccess = withAccessControl(Case, ['BHW']);
+const AdvanceViewPatientWithAccess = withAccessControl(AdvanceViewPatient, ['BHW']);
+const AddPatientContactWithAccess = withAccessControl(AddPatientContact, ['BHW']);
 
 function App() {
   return (
@@ -31,34 +59,37 @@ function App() {
       <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Login/>} />
+
+                    {/* admin routes */}
                     
-                    <Route path="/adminlabtest" element={<AdminLabTest/>}/>
-                    <Route path="/adminhi" element={<AdminHI/>}/>
-                    <Route path="/adminbhc" element={<AdminBHC/>}/>
-                    <Route path="/hi/:id" element={<HIInfo/>}/>
-                    <Route path="/bhc/:id" element={<BHCInfo/>}/>
-                    
-                    
-                    <Route path="/home" element={<PatientSummary/>}/>
-                    <Route path="/home1" element={<PatientSummaryPTB/>}/>
-                    <Route path="/home2" element={<PatientSummaryEPTB/>}/>
-                    <Route path="/home3" element={<PatientSummaryClosed/>}/>
+                    <Route path="/adminlabtest" element={<AdminLabTestwithAccess/>}/>
+                    <Route path="/adminhi" element={<AdminHIWithAccess/>}/>
+                    <Route path="/adminbhc" element={<AdminBHCWithAccess/>}/>
+                    <Route path="/hi/:id" element={<HIInfoWithAccess/>}/>
+                    <Route path="/bhc/:id" element={<BHCInfowithAccess/>}/>
+
+
+                    {/* BHW routes */}
+                    <Route path="/home"  element={<PatientSummaryWithAccess/>}/>
+                    <Route path="/home1" element={<PatientSummaryPTBWithAccess/>}/>
+                    <Route path="/home2" element={<PatientSummaryEPTBWithAccess/>}/>
+                    <Route path="/home3" element={<PatientSummaryClosedWithAccess/>}/>
 
 
 
-                    <Route path="/patient/:id" element={<PatientInfo/>}/>
-                    <Route path="/closecontacts/:id" element={<CloseContacts/>}/>
-                    <Route path="/assessment/:id" element={<Assessment/>}/>
-                    <Route path="/labtest/:id" element={<LaboratoryTest/>}/>
-                    <Route path="/diagnosis/:id" element={<Diagnosis/>}/>
-                    <Route path="/treatments/:id" element={<Treatments/>}/>
-                    <Route path="/similarcases/:id" element={<SimilarCases/>}/>
-                    <Route path="/addpatient" element={<AddPatient/>}/>
-                    <Route path="/allpatient" element={<ViewPatient/>}/>
+                    <Route path="/patient/:id" element={<PatientInfoWithAccess/>}/>
+                    <Route path="/closecontacts/:id" element={<CloseContactsWithAccess/>}/>
+                    <Route path="/assessment/:id" element={<AssessmentWithAccess/>}/>
+                    <Route path="/labtest/:id" element={<LaboratoryTestWithAccess/>}/>
+                    <Route path="/diagnosis/:id" element={<DiagnosisWithAccess/>}/>
+                    <Route path="/treatments/:id" element={<TreatmentsWithAccess/>}/>
+                    <Route path="/similarcases/:id" element={<SimilarCasesWithAccess/>}/>
+                    <Route path="/addpatient" element={<AddPatientWithAccess/>}/>
+                    <Route path="/allpatient" element={<ViewPatientWithAccess/>}/>
                     
-                    <Route path="/case/:id" element={<Case/>}/>
-                    <Route path="/allpatient/searchpatient/:lnm/:fnm/:mnm/:age/:sex/:bd/:nt/:phn/:ps/:pb/:pc/:pr/:pz/:chn/:cs/:cb/:cc/:cr/:cz/:ad/:mn/:mb/:mc/:me/:fn/:fb/:fc/:fe/:en/:eb/:ec/:ee" element={<AdvanceViewPatient/>}/>
-                    <Route path="/addpatient/:id" element={<AddPatientContact/>}/>
+                    <Route path="/case/:id" element={<CaseWithAccess/>}/>
+                    <Route path="/allpatient/searchpatient/:lnm/:fnm/:mnm/:age/:sex/:bd/:nt/:phn/:ps/:pb/:pc/:pr/:pz/:chn/:cs/:cb/:cc/:cr/:cz/:ad/:mn/:mb/:mc/:me/:fn/:fb/:fc/:fe/:en/:eb/:ec/:ee" element={<AdvanceViewPatientWithAccess/>}/>
+                    <Route path="/addpatient/:id" element={<AddPatientContactWithAccess/>}/>
 
                     <Route path="/test" element={<TestRoute/>}/>
                     
