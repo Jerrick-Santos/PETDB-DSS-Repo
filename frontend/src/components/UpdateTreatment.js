d1b17987f5563a6b7756331bd354c9cffde1400f
carlo-reyes <carlo_jose_reyes@dlsu.edu.ph>
2023-10-01 20:42:50 +0800

    Progress code for updating treatments

--- frontend/src/components/UpdateTreatment.js
+++ frontend/src/components/UpdateTreatment.js
@@ -13,77 +13,13 @@ function UpdateTreatment(props) {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const[regionData, setRegionData] = useState([])
-
-    useEffect(() => {
-
-        axios.get(`http://localhost:4000/api/allregions`)
-          .then((response) => {
-            setRegionData(response.data)
-          })
-          .catch((error) => {
-            // Handle any errors that occurred during the request
-            console.error('Error fetching data:', error);
-          });
-        
-    
-    }, []);
-
-    const [provinceData, setProvinceData] = useState([]);
-
-    const handleRegionChange = (e) => {
-        const selectedRegion = e.target.value;
-        axios.get(`http://localhost:4000/api/provinces/${selectedRegion}`)
-            .then((response) => {
-                setProvinceData(response.data);
-            })
-            .catch((error) => {
-                console.error('Error fetching provinces:', error);
-            });
-    }
-
-    const [cityData, setCityData] = useState([]);
-
-    const handleProvinceChange = (e) => {
-        const selectedProvince = e.target.value;
-        axios.get(`http://localhost:4000/api/cities/${selectedProvince}`)
-            .then((response) => {
-                setCityData(response.data);
-            })
-            .catch((error) => {
-                console.error('Error fetching cities:', error);
-            });
-    }
-
-    const [barangayData, setBarangayData] = useState([]);
-
-    const handleCityChange = (e) => {
-        const selectedCity = e.target.value;
-        axios.get(`http://localhost:4000/api/barangays/${selectedCity}`)
-        .then((response) => {
-            setBarangayData(response.data);
-        })
-        .catch((error) => {
-            console.error('Error fetching barangays:', error);
-        });
-    }
-
-
     const [formValues, setFormValues] = useState({
-        HIName: props.HIName,
-        HIOperatingHours:props.HIOperatingHours,
-        HIContactNumber:props.HIContactNumber,
-        HIEmailAddress:props.HIEmailAddress,
-        HIUnitNo:'',
-        HIStreet:'',
-        HIBarangay:'',
-        HICity:'',
-        HIRegion:'',
-        HIZipCode:'',
-        XCoord:props.XCoord,
-        YCoord:props.YCoord,
-        HIContactPerson: props.HIContactPerson,
-        isActive: 1
+        TreatmentID: props.TreatmentID,
+        Medicine: props.Medicine,
+        Dosage: props.Dosage,
+        Frequency: props.Frequency,
+        StartDate: props.StartDate,
+        EndDate: props.EndDate
     });
 
     const handleChange = (e) => {
@@ -94,7 +30,7 @@ function UpdateTreatment(props) {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try{
-            await axios.post("http://localhost:4000/api/newhi", formValues)
+            await axios.post("http://localhost:4000/api/updatetreatments", formValues)
         }catch(err){
             console.log(err)
         }
@@ -115,160 +51,36 @@ function UpdateTreatment(props) {
         <Modal.Title>Update Health Institution</Modal.Title>
     </Modal.Header>
     <Modal.Body>
-    <form className="mt-3 justify-content-center">
+        <form className="mt-3 justify-content-center">
+            <Row className="mb-3 justify-content-center">
+                <div className="form-group col-md-7">
+                    <label for="inputFirstName">Medicine Name</label>
+                    <input type="text" class="form-control" name="Medicine" value={formValues.Medicine} onChange={handleChange} placeholder="medicine name"/>
+                </div>
+                <div className="form-group col-md-5">
+                    <label for="inputFirstName">Dosage</label>
+                    <input type="text" class="form-control" name="Dosage" value={formValues.Dosage} onChange={handleChange} placeholder="dosage of medicine"/>
+                </div>
+            </Row>
             <Row className="mb-3 justify-content-center">
                 <div className="form-group col-md-12">
-                    <label for="inputFirstName">Health Institution Name</label>
-                    <input type="text" class="form-control"  name="HIName"  value={formValues.HIName} onChange={handleChange} placeholder="HI Name"/>
+                    <label for="inputFirstName">Frequency</label>
+                    <input type="text" class="form-control" name="Frequency" value={formValues.Frequency} onChange={handleChange} placeholder="frequency of intake"/>
                 </div>
             </Row>
-
             <Row className="mb-3 justify-content-center">
                 <div className="form-group col-md-12">
-                    <label for="inputOperatingHours">Operating Hours</label>
-                    <input type="text" class="form-control" name="HIOperatingHours" value={formValues.HIOperatingHours} onChange={handleChange} placeholder="Operating Hours"/>
+                    <label for="inputFirstName">Date Started</label>
+                    <input type="date" class="form-control" name="StartDate" value={formValues.StartDate} onChange={handleChange} />
                 </div>
             </Row>
-           
             <Row className="mb-3 justify-content-center">
-                <div className="form-group col-md-3">
-                    <label for="inputCurrHouseNo">Unit No.</label>
-                    <input type="text" class="form-control"  name='HIUnitNo' value={formValues.HIUnitNo} onChange={handleChange} placeholder="House No."/>
-                </div>
-                
-                <div className="form-group col-md-3">
-                    <label for="inputCurrStreet">Street</label>
-                    <input type="text" class="form-control" name='HIStreet' value={formValues.HIStreet} onChange={handleChange}  placeholder="Street"/>
-                </div>
-
-                <div className="form-group col-md-3">
-                    <label for="inputCurrRegion">Region</label>
-                    <select className="form-select" name="HIRegion" value={formValues.HIRegion} onChange={(e)=>{
-                        handleChange(e);
-                        handleRegionChange(e);
-                    }}>
-                        <option value="">Select</option>
-                    
-                    {regionData.map((hi, index) => (
-                    <>
-                    <option value={hi.region_id}>{hi.region_name}</option>
-                    
-                        </>
-                            ))}
-        
-
-                    </select>
-                </div>
-
-                <div className="form-group col-md-3">
-                    <label for="inputCurrRegion">Province</label>
-                    <select className="form-select" name="HIProvince" value={formValues.HIProvince} onChange={(e)=>{
-                        handleChange(e);
-                        handleProvinceChange(e);
-                    }}>
-                        <option value="">Select</option>
-                    
-                    {provinceData.map((hi, index) => (
-                    <>
-                    <option value={hi.province_id}>{hi.province_name}</option>
-                    
-                        </>
-                            ))}
-        
-
-                    </select>
-                </div>
-
-             
-            </Row>
-
-            <Row className="mb-3 justify-content-center">
-           
-            <div className="form-group col-md-4">
-                    <label for="inputCurrCity">City</label>
-                    <select className="form-select" name="HICity" value={formValues.HICity} onChange={(e)=>{
-                        handleChange(e);
-                        handleCityChange(e);
-                    }}>
-                        <option value="">Select</option>
-                    
-                    {cityData.map((hi, index) => (
-                    <>
-                    <option value={hi.municipality_id}>{hi.municipality_name}</option>
-                    
-                        </>
-                            ))}
-        
-
-                    </select>
-                </div>
-                
-               
-            <div class="form-group col-md-4">
-                    <label for="inputCurrBarangay">Barangay</label>
-                    <select className="form-select" name="HIBarangay" value={formValues.HIBarangay} onChange={handleChange}>
-                        <option value="">Select</option>
-                    
-                    {barangayData.map((hi, index) => (
-                    <>
-                    <option value={hi.barangay_id}>{hi.barangay_name}</option>
-                    
-                        </>
-                            ))}
-        
-
-                    </select>
-                </div>
-                
-
-               
-
-                <div className="form-group col-md-4">
-                    <label for="inputCurrZip">Zip Code</label>
-                    <input type="text" class="form-control"  name='HIZipCode' value={formValues.HIZipCode} onChange={handleChange}  placeholder="Zip"/>
-                </div>
-                
-            </Row>
-
-            <Row className="mb-3 justify-content-center">
-           
-
-           <div className="form-group col-md-6">
-               <label for="inputCurrCity">X Coordinate</label>
-               <input type="number" class="form-control"  name='XCoord' value={formValues.XCoord} onChange={handleChange}  placeholder="Coordinate"/>
-           </div>
-
-           <div className="form-group col-md-6">
-               <label for="inputCurrRegion">Y Coordinate</label>
-               <input type="number" class="form-control"  name='YCoord' value={formValues.YCoord} onChange={handleChange} placeholder="Coordinate"/>
-           </div>
-
-         
-           
-       </Row>
-
-       <Row className="mb-3 justify-content-center">
                 <div className="form-group col-md-12">
-                    <label for="inputOperatingHours">Contact Person</label>
-                    <input type="text" class="form-control" name="HIContactPerson" value={formValues.HIContactPerson} onChange={handleChange} placeholder="Name"/>
+                    <label for="inputFirstName">Date Ended</label>
+                    <input type="date" class="form-control" name="EndDate" value={formValues.EndDate} onChange={handleChange} />
                 </div>
             </Row>
-
-            <Row className="mb-3 justify-content-center">
-            <div class="form-group col-md-6">
-                    <label for="inputContactNumber">Contact #</label>
-                    <input type="text" class="form-control" name="HIContactNumber" value={formValues.HIContactNumber} onChange={handleChange} placeholder="e.g. 09xx-xxx-xxxx"/>
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="inputContactEmail">Email Address</label>
-                    <input type="text" class="form-control" name="HIEmailAddress" value={formValues.HIEmailAddress} onChange={handleChange} placeholder="e.g. sample@sample.com"/>
-                </div>
-            </Row>
-
-          
-
-            
-            </form>
+        </form>
     </Modal.Body>
     <Modal.Footer >
         <button className="btn" onClick={handleSubmit} style={{color:'white', backgroundColor: "#0077B6"}}>Add</button>
