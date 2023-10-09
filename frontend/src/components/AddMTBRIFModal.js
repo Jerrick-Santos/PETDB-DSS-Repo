c38afa46f6f0473baec4f9cb8a8187650ad42ac1
carlo-reyes <carlo_jose_reyes@dlsu.edu.ph>
2023-10-09 22:50:33 +0800

    Updated form validation for updating diagnostic tests: MTB/RIF

--- frontend/src/components/AddMTBRIFModal.js
+++ frontend/src/components/AddMTBRIFModal.js
@@ -4,6 +4,7 @@ import edit from '../assets/edit.png';
 import { Navbar, Nav, Card, Row, Col  } from 'react-bootstrap';
 import axios from 'axios';
 import add from '../assets/add.png';
+import Form from 'react-bootstrap/Form';
 function AddMTBRIFModal(props) {
    
     const[show,setShow] = useState(false)
@@ -26,9 +27,7 @@ function AddMTBRIFModal(props) {
         axios.get(`http://localhost:4000/api/validity/2`)
           .then((response) => {
           setMTBValidity(response.data)
-          console.log('Validity Months: ', mtbValidity);
-          // Call computeValidity after fetching xrayValidity and when issue_date changes
-          computeValidity();
+
           })
           .catch((error) => {
           // Handle any errors that occurred during the request
@@ -87,20 +86,22 @@ function AddMTBRIFModal(props) {
         setHIError(HINoError);
 
         let dateError = '';
-        if (!mtbValues.issue_date) {
-            dateError = 'Required';
+        if (new Date(mtbValues.issue_date).toLocaleDateString() === new Date().toLocaleDateString()) {
+            dateError = 'Please select a date';
         }
         setDateError(dateError);
 
         let testError = '';
         if (!mtbValues.test_refno) {
             testError = 'Required';
+        } else if (mtbValues.test_refno.length > 45) {
+            testError = 'Reference Number should not exceed 45 characters';
         }
         setTestError(testError);
 
         let valueError = '';
         if (!mtbValues.TestValue) {
-            valueError = 'Required';
+            valueError = 'Please select an option';
         }
         setValueError(valueError);
 
@@ -116,26 +117,6 @@ function AddMTBRIFModal(props) {
         setMTBValues((prev)=>({...prev, [name]: value}));
     }
 
-    const computeValidity = () => {
-        const today = new Date();
-        const issueDate = new Date(mtbValues.issue_date);
-
-        if (mtbValidity.length > 0) {
-            const validityMonths = mtbValidity[0].DGValidityMonths;
-            const validityExpirationDate = new Date(issueDate);
-            validityExpirationDate.setMonth(validityExpirationDate.getMonth() + validityMonths);
-
-            console.log('Today: ', today);
-            console.log('issueDate: ', issueDate);
-            console.log("Computed Validity: ", today > validityExpirationDate ? 0 : 1);
-
-            if (today <= validityExpirationDate) {
-                setMTBValues((prev) => ({ ...prev, validity: 1 }));
-              } else {
-                setMTBValues((prev) => ({ ...prev, validity: 0 }));
-              }
-        }
-    };
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -175,7 +156,73 @@ function AddMTBRIFModal(props) {
         <Modal.Title>Add MTB/RIF Results</Modal.Title>
     </Modal.Header>
     <Modal.Body>
-        <div>
+        <Form noValidate onSubmit={handleSubmit}>
+            <Row className="mb-3 justify-content-center">
+                {/* For HI Number */}
+                <Form.Group as={Col} md="12" className='mb-3' controlId='HINo'>
+                    <Form.Label><strong>Issued by:</strong></Form.Label>
+                    <Form.Select
+                        aria-label="HINo"
+                        name='HINo'
+                        value={mtbValues.HINo}
+                        onChange={handleChange}
+                        isInvalid={HINoError}>
+                            <option value="">Select</option>
+              
+                                {hiData.map((hi, index) => (
+                                <>
+                                <option value={hi.HINo}>{hi.HIName}</option>
+                                
+                                    </>
+                                ))}
+                    </Form.Select>
+                    <Form.Control.Feedback type='invalid'>{HINoError}</Form.Control.Feedback>
+                </Form.Group>
+                {/* For Issue Date */}
+                <Form.Group as={Col} md="12" className='mb-3' controlId='issue_date'>
+                    <Form.Label><strong>Issued on:</strong></Form.Label>
+                    <Form.Control
+                        required
+                        type='date'
+                        name='issue_date'
+                        onChange={handleChange}
+                        value={new Date(mtbValues.issue_date).toISOString().split('T')[0]}
+                        isInvalid={dateError}
+                    />
+                    <Form.Control.Feedback type='invalid'>{dateError}</Form.Control.Feedback>
+                </Form.Group>
+                {/* For Reference Number */}
+                <Form.Group as={Col} md="12" className='mb-3' controlId='test_refno'>
+                    <Form.Label><strong>Reference Number:</strong></Form.Label>
+                    <Form.Control
+                        required
+                        type='text'
+                        name='test_refno'
+                        onChange={handleChange}
+                        value={mtbValues.test_refno}
+                        isInvalid={testError}
+                    />
+                    <Form.Control.Feedback type='invalid'>{testError}</Form.Control.Feedback>
+                </Form.Group>
+                {/* For Test Value */}
+                <Form.Group as={Col} md="12" controlId='TestValue'>
+                    <Form.Label><strong>Xray Results:</strong></Form.Label>
+                    <Form.Select
+                        aria-label="TestValue"
+                        name='TestValue'
+                        value={mtbValues.TestValue}
+                        onChange={handleChange}
+                        isInvalid={valueError}>
+                            <option value="">Select</option>
+                            <option value="With Signs of TB">With Signs of TB</option>
+                            <option value="No signs">No signs</option>
+                            <option value="Undetermined">Undetermined</option>
+                    </Form.Select>
+                    <Form.Control.Feedback type='invalid'>{valueError}</Form.Control.Feedback>
+                </Form.Group>
+            </Row>
+        </Form>
+        {/*<div>
             <label><strong> Upload MTB/RIF File Attachment:</strong></label>
             <input type="file" className="form-control" />
         </div>
@@ -226,7 +273,7 @@ function AddMTBRIFModal(props) {
             {valueError && (
                 <p style={{color: 'red'}}>{valueError}</p>  
             )}
-        </div>
+            </div>*/}
     </Modal.Body>
     <Modal.Footer >
         <button className="btn" onClick={handleSubmit} style={{color:'white', backgroundColor: "#0077B6"}}>Save</button>
