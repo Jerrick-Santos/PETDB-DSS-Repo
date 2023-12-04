e99c027b2d978c11e444596277b776203d0865c2
carlo-reyes <carlo_jose_reyes@dlsu.edu.ph>
2023-12-05 03:30:47 +0800

    Changed browser tab name to DetecTB

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+document.title = 'DetecTB';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
