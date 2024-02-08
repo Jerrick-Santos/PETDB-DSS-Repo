f89e14de9552b3190a136bff35dca87413404d71
raymondintervalo <raymondmatthewintervalo@gmail.com>
2024-02-08 23:56:07 +0800

    up

--- sqlscripts/create-db.bat
+++
@@ -1,2 +0,0 @@
-
-mysql -u root -p 12345 > pedtb_11_20_23_clean.sql
--- sqlscripts/install_dependencies.bat
+++
@@ -1,7 +0,0 @@
-:: Install Node.js using NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-nvm install node
-
-:: Install MySQL Server and MySQL Shell (using MySQL Installer)
-:: Modify the path to the installer based on your downloaded version
-start /wait "MySQL Installer" "C:\path\to\mysql-installer-community-version.msi"
